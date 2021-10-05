591c24e5bd403d1a70faeb4d9b62bf71a3d665f2
Leandro Solagna <leandrosolagna@gmail.com>
2021-10-05 20:51:47 +0200

    added variables file

---
+++ aws/variables.tf
@@ -0,0 +1,11 @@
+variable "profile" {
+  type = string
+  description = "My AWS profile"
+  default = "terraformbook"
+}
+
+variable "region" {
+  type = string
+  description = "My AWS region"
+  default = "eu-central-1"
+}
