317caca9ef0278e17abb77b31d86edd593da4ce5
Leandro Solagna <leandrosolagna@gmail.com>
2021-10-05 20:51:29 +0200

    added provider to main file

--- aws/main.tf
+++ aws/main.tf
@@ -1,3 +1,12 @@
+provider "aws" {
+  profile = var.profile
+  region  = var.region
+}
+
 module "networking" {
   source = "./modules/networking"
+}
+
+module "machines" {
+  source = "./modules/machines"
 }
