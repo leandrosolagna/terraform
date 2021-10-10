fd3002658a3828b9f7e71326ee75a600bdc21d7b
Leandro Solagna <leandrosolagna@gmail.com>
2021-10-10 20:57:41 +0200

    added variables and another module to main

---
+++ aws/variables.tf
@@ -0,0 +1,11 @@
+variable "profile" {
+  type = string
+  description = "My AWS profile"
+  default = "leandroiam"
+}
+
+variable "region" {
+  type = string
+  description = "My AWS region"
+  default = "eu-central-1"
+}
