8ffba488ac46a67584440f1ff208c5aa417164a8
Leandro Solagna <leandrosolagna@gmail.com>
2021-10-12 21:44:37 +0200

    added route and gateway to networking

--- aws/modules/networking/main.tf
+++ aws/modules/networking/main.tf
@@ -12,3 +12,29 @@ module "vpc" {
     Name = "net-ec2"
   }
 }
+
+resource "aws_internet_gateway" "net-igw" {
+  vpc_id = module.vpc.vpc_id
+
+  tags = {
+    Name = "net-igw-ec2"
+  }
+}
+
+resource "aws_route_table" "net-route-table" {
+  vpc_id = module.vpc.vpc_id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.net-igw.id
+  }
+
+  tags = {
+    Name = "net-route-table-ec2"
+  }
+}
+
+resource "aws_route_table_association" "net-route-association-1" {
+  subnet_id = module.vpc.public_subnets[0]
+  route_table_id = aws_route_table.net-route-table.id
+}
