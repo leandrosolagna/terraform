4a2645b270d672c21a6e8d7d25ea9ca0ebe0403b
Leandro Solagna <leandrosolagna@gmail.com>
2021-10-13 20:04:15 +0200

    Feature/adding route to network (#4)
    * route_table_association removed

--- aws/modules/networking/main.tf
+++ aws/modules/networking/main.tf
@@ -12,3 +12,16 @@ module "vpc" {
     Name = "net-ec2"
   }
 }
+
+resource "aws_route_table" "net-route-table" {
+  vpc_id = module.vpc.vpc_id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = module.vpc.igw_id
+  }
+
+  tags = {
+    Name = "net-route-table-ec2"
+  }
+}
