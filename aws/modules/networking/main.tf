85c59d3966798b533d2ba68558747ed68e03fa3b
Leandro Solagna <leandrosolagna@gmail.com>
2021-10-26 21:19:05 +0200

    networking main

--- aws/modules/networking/main.tf
+++ aws/modules/networking/main.tf
@@ -1,11 +1,11 @@
 module "vpc" {
-  source          = "terraform-aws-modules/vpc/aws"
-  azs             = ["${var.region}a"]
-  name            = var.vpc_name
-  cidr            = "10.0.0.0/16"
-  private_subnets = ["10.0.1.0/24"]
-  public_subnets  = ["10.0.100.0/24"]
-  enable_dns_hostnames = true
+  source                  = "terraform-aws-modules/vpc/aws"
+  azs                     = ["${var.region}a"]
+  name                    = var.vpc_name
+  cidr                    = "10.0.0.0/16"
+  private_subnets         = ["10.0.1.0/24"]
+  public_subnets          = ["10.0.100.0/24"]
+  enable_dns_hostnames    = true
   map_public_ip_on_launch = true
 
   tags = {
