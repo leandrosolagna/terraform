c0a7f9f654c604b5c9ce29c59a69c6067546304e
Leandro Solagna <leandrosolagna@gmail.com>
2021-10-28 21:36:24 +0200

    added backend s3

--- aws/main.tf
+++ aws/main.tf
@@ -3,6 +3,15 @@ provider "aws" {
   profile = var.profile
 }
 
+terraform {
+  backend "s3" {
+    profile = "leandroiam"
+    region  = "eu-central-1"
+    bucket  = "state-bucket-solagna"
+    key     = "terraform.tfstate"
+  }
+}
+
 module "networking" {
   source   = "./modules/networking"
   region   = var.region
